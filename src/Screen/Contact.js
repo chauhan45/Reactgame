e758a3a0f37784700f7d9c977544cb4c230b5023
amit <sonusingh86201.com>
2022-04-26 18:18:43 +0530

    changes

--- src/Screen/Contact.js
+++ src/Screen/Contact.js
@@ -117,6 +117,39 @@ function Contact() {
           </div>
         </div>
       </section>
+      <footer>
+        <div class="flex">
+          <small>
+            Copyright &copy; 2022 All rights reserved  | This Website is made
+            with <span class="footer-heart">&#9829;</span> by{" "}
+            <a href="https://truintenttechnologies.com/" target="_blank">
+            truintenttechnologies
+            </a>
+            .
+          </small>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+
+            <li>
+              <a href="#">Games</a>
+            </li>
+
+            <li>
+              <a href="#">Blog</a>
+            </li>
+
+            <li>
+              <a href="#">Forums</a>
+            </li>
+
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
